1746b307a4576436496c975e09a906934cefa10c
liuyunxiao <liuyunxiao-lyx@163.com>
2013-06-23 03:30:07 +0800

    no message

---
+++ OGame/UIDirButton.h
@@ -0,0 +1,13 @@
+//
+//  UIDirButton.h
+//  OGame
+//
+//  Created by liuyunxiao on 13-6-23.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDirButton : UIButton
+
+@end
---
+++ OGame/UIDirButton.mm
@@ -0,0 +1,31 @@
+//
+//  UIDirButton.m
+//  OGame
+//
+//  Created by liuyunxiao on 13-6-23.
+//
+//
+
+#import "UIDirButton.h"
+
+@implementation UIDirButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
